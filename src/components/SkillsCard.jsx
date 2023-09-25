c363b62cf9e42969f2c9b1900241892455b322a8
Koyel Das <koyel.das@sentientgeeks.com>
2023-09-25 14:48:59 +0530

    first commit

---
+++ src/components/SkillsCard.jsx
@@ -0,0 +1,12 @@
+const SkillsCard = ({id, title, icon, text}) => {
+  return (
+    <article key={id}>
+        <span>{icon}</span>
+        <h4 className="mt-6 font-bold ">
+            {title}
+        </h4>
+        <p className="mt-2 text-slate-500">{text}</p>
+    </article>
+  )
+}
+export default SkillsCard
